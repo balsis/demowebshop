cfeff4071d4f421a6b292d18d24c2466226a0793
artur.balsis <artur.balsis@gmail.com>
2025-04-09 20:59:11 +0300

    edit fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,17 @@
-import time
-
 import allure
 import pytest
 from selene import browser
-from selene.support import webdriver
 
 from demowebshop import app
+from demowebshop.data.data import Data
 from demowebshop.helpers import logger
 
 
 @pytest.fixture(scope = "function")
-def authorised_user(setup_browser):
+def authorised_user(quit_browser):
     with allure.step("Выставление cookies для авторизованного пользователя"):
         auth_cookie = app.api.auth_cookie
-        browser.open('https://demowebshop.tricentis.com/')
+        browser.open(Data.BASE_URL)
         browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": auth_cookie})
         logger.basic_logger.info(f"Cookie для авторизованного пользователя выставлена: {auth_cookie[:9]}")
         yield browser
@@ -23,10 +21,5 @@ def authorised_user(setup_browser):
 
 @pytest.fixture(scope = "function", autouse = True)
 @allure.step("Инициализация браузера")
-def setup_browser():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    options = webdriver.ChromeOptions()
-    options.add_argument('--incognito')
-    yield
-    browser.quit()
+def quit_browser():
+    yield browser.quit()
